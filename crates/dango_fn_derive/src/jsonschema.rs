277dd5a8fa884cde3cb215a5f0b962e88a7a92b5
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-06-29 20:45:19 +0900

    construct string

--- crates/dango_fn_derive/src/jsonschema.rs
+++ crates/dango_fn_derive/src/jsonschema.rs
@@ -1,10 +1,16 @@
+use proc_macro::TokenStream;
+use quote::quote;
 use serde_json::{json, Value};
+use syn::Expr;
 
-pub fn string(description: String) -> Value {
-    json! {
-        {
-            "type": "string",
-            "description": description
+pub fn string(description: Expr) -> TokenStream {
+    quote! {
+        json! {
+            {
+                "type": "string",
+                "description": #description
+            }
         }
     }
+    .into()
 }
--- crates/dango_fn_derive/src/lib.rs
+++ crates/dango_fn_derive/src/lib.rs
@@ -4,7 +4,7 @@ use std::any::{Any, TypeId};
 
 use proc_macro::TokenStream;
 use quote::quote;
-use serde_json::Value;
+use serde_json::{json, Value};
 use syn::{parse::Parse, parse_macro_input, Expr, Fields, Ident, Item, ItemStruct, Meta};
 
 mod jsonschema;
@@ -28,11 +28,11 @@ fn impl_function(item: ItemStruct) -> TokenStream {
 
     match item.fields {
         Fields::Named(ref inner) => {
-            let fields: Vec<(String, Value)> = inner
+            let fields: Vec<(String, TokenStream)> = inner
                 .named
                 .iter()
                 .map(|field| {
-                    let field_name = field.ident.as_ref().unwrap();
+                    let field_name = field.ident.as_ref().unwrap().to_string();
 
                     // filter all doc comments and concat them?
                     let description = field
@@ -41,18 +41,27 @@ fn impl_function(item: ItemStruct) -> TokenStream {
                         .find_map(|attr| {
                             if attr.path().is_ident("doc") {
                                 let doc: Expr = attr.parse_args().unwrap();
+                                Some(doc)
+                            } else {
+                                None
                             }
-                            todo!()
                         })
                         .expect("Expected description");
 
-                    if field.type_id() == TypeId::of::<String>() {
-                        jsonschema::string(description);
-                    }
+                    let schema = if field.type_id() == TypeId::of::<String>() {
+                        jsonschema::string(description)
+                    } else {
+                        quote! { compile_error!("Unexpected Type") }.into()
+                    };
 
-                    todo!()
+                    (field_name, schema)
                 })
                 .collect();
+
+            let properties = quote! {
+                Map::from_iter()
+            };
+
             quote! {
                 json! {
                     "name": struct_name,
@@ -60,7 +69,6 @@ fn impl_function(item: ItemStruct) -> TokenStream {
                     "parameters": {
                         "type": "object",
                         "properties": {
-
                         }
                     }
                 }
